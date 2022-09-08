771226b84447bf4870bda56a8ffc822fe4dcac02
Huzaif_ <huzaifahmed0@gmail.com>
2022-09-08 10:39:06 +0500

    Added Students OCollection

--- FluentNgo/ViewModels/StudentViewModel.cs
+++ FluentNgo/ViewModels/StudentViewModel.cs
@@ -1,8 +1,22 @@
 ﻿using FluentNgo.Core;
+using FluentNgo.Models;
+using System.Collections.ObjectModel;
 
 namespace FluentNgo.ViewModels
 {
     public class StudentViewModel : ObservableObject
     {
+        private ObservableCollection<Student> _students;
+        public ObservableCollection<Student> Students
+        {
+            get { return _students; }
+            set
+            {
+                if (value == _students)
+                    return;
+                _students = value;
+                OnPropertyChanged("Name");
+            }
+        }
     }
 }
