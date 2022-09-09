5a3df293248a480b639103bcff9e810abbae115b
Huzaif_ <huzaifahmed0@gmail.com>
2022-09-09 10:14:45 +0500

    Added GetById Method

--- FluentNgo/Models/Student.cs
+++ FluentNgo/Models/Student.cs
@@ -1,37 +1,14 @@
 ﻿using Dapper;
 using System.Data.SQLite;
 using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System;
 
 namespace FluentNgo.Models
 {
     public class Student
     {
-        public Student() { }
-
-        public Student(int gr_no, string name, string f_name, string m_name, string grade_class,
-            string dob, string address, string phone_no, string religion, string nic, string admission_date,
-            int t_family_members, string f_occupation, string first_admitted_class, string email,
-            string schooling_type, string grades_results)
-        {
-            GrNo = gr_no;
-            Name = name;
-            FatherName = f_name;
-            MotherName = m_name;
-            Class = grade_class;
-            DOB = dob;
-            Address = address;
-            PhoneNumber = phone_no;
-            Religion = religion;
-            NIC = nic;
-            DateOfAdmission = admission_date;
-            TotalFamilyMembers = t_family_members;
-            FathersOccupation = f_occupation;
-            FirstAdmittedClass = first_admitted_class;
-            Email = email;
-            SchoolingType = schooling_type;
-            Grades = grades_results;
-        }
-
         public int GrNo { get; set; }
         public string Name { get; set; }
         public string FatherName { get; set; }
@@ -53,11 +30,32 @@ namespace FluentNgo.Models
 
         public static List<Student> StudentGetAll()
         {
-            using (var Connection = new SQLiteConnection(App.ConnectionString))
+            var Connection = new SQLiteConnection(App.ConnectionString);
+            Connection.Open();
+            try
             {
                 var output = Connection.Query<Student>("SELECT * FROM Students", new DynamicParameters());
 
                 return output.AsList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                Connection.Close();
+            }
+            return new List<Student>();
+        }
+
+        public static Student StudentGetById(int Id)
+        {
+            using (var Connection = new SQLiteConnection(App.ConnectionString))
+            {
+                var output = Connection.Query<Student>($"SELECT * FROM Students WHERE GrNo = {Id}", new DynamicParameters()).First();
+
+                return output;
             }
         }
     }
