55b00e17a1a19f2ccec8a23ab258c0f8664cd87b
Huzaif0_ <huzaifahmed0@gmail.com>
2022-09-20 20:05:52 +0500

    Incomplete Form

---
+++ FluentNgo/Views/Components/BeneficiaryForm.xaml.cs
@@ -0,0 +1,54 @@
+﻿using FluentNgo.Models;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace FluentNgo.Views.Components
+{
+    /// <summary>
+    /// Interaction logic for BeneficiaryForm.xaml
+    /// </summary>
+    public partial class BeneficiaryForm : Window
+    {
+        public Beneficiary beneficiary { get; set; }
+
+        public BeneficiaryForm()
+        {
+            InitializeComponent();
+            beneficiary = new Beneficiary();
+            DataContext = this;
+        }
+
+        public BeneficiaryForm(Beneficiary beneficiary)
+        {
+            InitializeComponent();
+            this.beneficiary = beneficiary;
+            DataContext = this;
+        }
+
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void SubmitForm(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void BF_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            RootGrid.Focus();
+        }
+
+        private void HandleExpanders(object sender, RoutedEventArgs e)
+        {
+            foreach (var child in ExpanderPannel.Children)
+            {
+                if (child is Expander && child != sender as Expander)
+                    ((Expander)child).IsExpanded = false;
+            }
+        }
+    }
+}
